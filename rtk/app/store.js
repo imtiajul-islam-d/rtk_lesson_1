75a5dd21bd7b1ae0ae817f87649124f3cfce006f
imtiajul-islam-d <imtiajul@hotmail.com>
2023-03-04 21:59:00 +0600

    rtk store added

---
+++ rtk/app/store.js
@@ -0,0 +1,10 @@
+const counterReducer = require("../../redux/counter/reducer");
+const configureStore = require("@reduxjs/toolkit").configureStore;
+
+// configure store
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+module.export = store;
