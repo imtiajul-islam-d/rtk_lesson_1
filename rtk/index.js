9ab26240a137d4b69f79ea74013e281e83b5008a
imtiajul-islam-d <imtiajul@hotmail.com>
2023-03-04 22:30:07 +0600

    initial state state added on store

--- rtk/index.js
+++ rtk/index.js
@@ -1,5 +1,8 @@
 const store = require("./app/store");
 const {counterActions} = require("./features/counter/counterSlice")
+
+// initial state
+console.log(`Initial State: ${JSON.stringify(store.getState())}`);
 // subscribe to state changes
 store.subscribe(() => {
     console.log(store.getState());
