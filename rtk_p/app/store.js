6e278b8450899adf020c02510ac202aa9c8f8444
imtiajul-islam-d <imtiajul@hotmail.com>
2023-03-05 15:06:46 +0600

    practice part added

--- rtk_p/app/store.js
+++ rtk_p/app/store.js
@@ -1,10 +1,10 @@
+const { configureStore } = require("@reduxjs/toolkit");
 const counterReducer = require("./../features/counter/counterSlice");
-const configureStore = require("@reduxjs/toolkit").configureStore;
 
-// configure store
 const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
 });
+
 module.exports = store;
